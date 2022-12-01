0c0715945b7f0ade022995d8eb925654dc101de7
George <71167099+letow@users.noreply.github.com>
2022-12-01 20:11:26 +0500

    replaced .css with .scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./App.scss";
 import TodoList from "./Components/TodoList/TodoList";
 import Login from "./Components/Login/Login";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
