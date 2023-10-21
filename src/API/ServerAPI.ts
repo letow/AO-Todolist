bb699c5cb13568f1b4994a8a254c1f9aeaa529e5
George <71167099+letow@users.noreply.github.com>
2023-10-22 01:17:54 +0300

    fixed server link

--- src/API/ServerAPI.ts
+++ src/API/ServerAPI.ts
@@ -2,19 +2,19 @@ import { LoginInfo } from "../Types/LoginInfo";
 import { Todo } from "../Types/Todo";
 
 export const getAllItems = async (queryParam: string, hash: string) => {
-  const url = `http://localhost:8000/todos?hash=${hash}`;
+  const url = `http://my-json-server.typicode.com/letow/ao-db/todos?hash=${hash}`;
   return await fetch(
     url + (queryParam ? `&done=${queryParam.toString()}` : "")
   ).then((response) => response.json());
 };
 
 export const getItem = async (id: number) => {
-  const url = `http://localhost:8000/todos/${id}`;
+  const url = `http://my-json-server.typicode.com/letow/ao-db/todos/${id}`;
   return await fetch(url).then((response) => response.status);
 };
 
 export const addItem = async (data: Todo, hash: string) => {
-  const url = `http://localhost:8000/todos`;
+  const url = `http://my-json-server.typicode.com/letow/ao-db/todos`;
   return await fetch(url, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
@@ -23,14 +23,14 @@ export const addItem = async (data: Todo, hash: string) => {
 };
 
 export const removeItem = async (data: number, hash: string) => {
-  const url = `http://localhost:8000/todos/${data}?hash=${hash}`;
+  const url = `http://my-json-server.typicode.com/letow/ao-db/todos/${data}?hash=${hash}`;
   return await fetch(url, {
     method: "DELETE",
   }).then((response) => response.json());
 };
 
 export const doneItem = async (data: Todo, hash: string) => {
-  const url = `http://localhost:8000/todos?hash=${hash}`;
+  const url = `http://my-json-server.typicode.com/letow/ao-db/todos?hash=${hash}`;
   return await fetch(url + `/${data.id}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json", id: data.id.toString() },
@@ -39,6 +39,6 @@ export const doneItem = async (data: Todo, hash: string) => {
 };
 
 export const signIn = async (data: LoginInfo) => {
-  const url = `http://localhost:8000/users?login=${data.login}&hash=${data.hash}`;
+  const url = `http://my-json-server.typicode.com/letow/ao-db/users?login=${data.login}&hash=${data.hash}`;
   return await fetch(url).then((response) => response.json());
 };
